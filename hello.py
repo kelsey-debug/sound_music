b2636fb85759f02cd863a157a30fbd9a4d05d031
kelsey-debug <kelsey36@pdx.edu>
2021-04-04 14:49:01 -0700

    simpleaudio lib makes a noise using hw specs

--- hello.py
+++ hello.py
@@ -1,9 +1,28 @@
 
-#import simpleaudio as sa 
-from playsound import playsound
+import simpleaudio as sa 
+import numpy as num
+import matplotlib.pyplot as pyplot
 
-playsound('sine.wav') //currently empty
+duration = 3 
+sample_rate = 48000 #samples per second 
+freq = 440 * 2 * num.pi #in radians
 
+#generate array holding timesteps.start at 0 and end at 3 seconds, containing samplerate * duration num of samples.to have enough components sampled for hifidelity? 
+timestep = num.linspace (0,duration,sample_rate * duration, False)
 
-msg = "hello"
-print(msg)
+sin_wave = num.sin(freq * timestep ) #no phase specified in hw
+
+#2^15 - 1 = 32767 is the full amp range. I want my amplitude to be limited to a quarter this size (32767 *.25 = 8191). 
+audio = sin_wave * 8191/ num.max(num.abs(sin_wave))
+#audio = sin_wave * 32767/ num.max(num.abs(sin_wave))  this is the normal. high quality amp range we want to be inside
+
+#convert wave to 16 bit integers
+sound = audio.astype(num.int16)
+
+wav_obj = sa.WaveObject(sound,1,2,sample_rate)
+playback_obj = wav_obj.play() 
+
+if playback_obj.is_playing():
+    print('still playing')
+
+playback_obj.wait_done() 
